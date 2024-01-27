9afccf7f2a4478cb1b3dc9ede4404f878e1218d3
harshitzzzzz <harshupreti567@gmail.com>
2024-01-27 19:06:47 +0530

    text to speech

---
+++ index.js
@@ -0,0 +1,18 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = function () {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+voiceSelect.addEventListener("chnage",function(){
+    speech.voice=voices[voiceSelect.value]
+})
+
+document.querySelector("button").addEventListener('click', function () {
+    speech.text = document.querySelector("textarea").value;
+    speech.voice = voices[voiceSelect.selectedIndex];
+    window.speechSynthesis.speak(speech);
+});
